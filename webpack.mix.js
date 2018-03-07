c6735a1c050a37ea6f63dd53e8b4fbac652e6c1e
Goopil <zac@goopil.org>
2018-03-07 23:37:51 +0100

    refactor mix config. to handle async modules

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,39 +1,31 @@
-const path = require('path')
-const webpack = require('webpack')
 const mix = require('laravel-mix')
-// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
+const webpackAlias = require('./webpack.config')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
 mix
   .js('resources/assets/js/app.js', 'public/js')
   .stylus('resources/assets/stylus/app.styl', 'public/css')
   .sourceMaps()
   .disableNotifications()
-  .copyDirectory('resources/assets/img', 'public/img')
-
-if (mix.inProduction()) {
-  mix.version()
-
-  mix.extract([
+  // .copyDirectory('resources/assets/img', 'public/img')
+  .version()
+  .extract([
     'vue',
-    'vform',
     'axios',
     'vuex',
     'vue-i18n',
-    'vue-meta',
-    'js-cookie',
     'vue-router',
-    'vuetify',
-    'vee-validate',
-    'vuex-router-sync'
+    'vuex-router-sync',
+    'vuetify'
   ])
-}
 
 mix.webpackConfig({
+  ...webpackAlias,
   plugins: [
+    new BundleAnalyzerPlugin()
   ],
-  resolve: {
-    alias: {
-      '~': path.join(__dirname, './resources/assets/js')
-    }
+  output: {
+    chunkFilename: 'js/[name].js',
+    publicPath: mix.config.hmr ? '//localhost:8080' : '/'
   }
 })
