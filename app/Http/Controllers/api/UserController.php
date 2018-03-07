2312da7a08310d71a5abc4a2ca99bf2b4ad1f14b
Goopil <zac@goopil.org>
2018-03-07 23:45:01 +0100

    add user sample controller

---
+++ app/Http/Controllers/api/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+
+    public function destroy ($id) {
+        User::destroy($id);
+
+        return response()->json();
+    }
+
+    public function show ($id)
+    {
+        $user = User::whereId($id)->first();
+        return response()->json($user);
+    }
+}
