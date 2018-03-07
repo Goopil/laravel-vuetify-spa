648a077e9d743fb53f83e868b90bef7759147c29
Goopil <zac@goopil.org>
2018-03-07 23:37:22 +0100

    refactor api routes

--- routes/api.php
+++ routes/api.php
@@ -13,20 +13,30 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::group(['middleware' => 'auth:api'], function () {
-    Route::post('logout', 'Auth\LoginController@logout');
+Route::get('translations/locale/{lang}', 'TranslationController@showPerLang');
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+Route::group([
+    'middleware' => 'auth:api'
+], function () {
+    Route::post('logout', 'Auth\LoginController@logout');
+    Route::get('user', 'Auth\LoginController@user');
+
+    Route::resource('translations', 'TranslationController');
+    Route::resource('users', 'UserController');
 
     Route::patch('settings/profile', 'Settings\UpdateProfile');
     Route::patch('settings/password', 'Settings\UpdatePassword');
 });
 
-Route::group(['middleware' => 'guest:api'], function () {
+Route::group([
+    'middleware' => 'guest:api'
+], function () {
     Route::post('login', 'Auth\LoginController@login');
     Route::post('register', 'Auth\RegisterController@register');
     Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail');
     Route::post('password/reset', 'Auth\ResetPasswordController@reset');
+
+    Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
+    Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
 });
+
