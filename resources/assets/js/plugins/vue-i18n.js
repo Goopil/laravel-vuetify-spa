7dea2a29bce2fddd35d55ff21b07ec1f0c9300f0
Goopil <zac@goopil.org>
2018-03-07 23:40:28 +0100

    add async load methods (from inital state or load them)

--- resources/assets/js/plugins/vue-i18n.js
+++ resources/assets/js/plugins/vue-i18n.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
+import axios from 'axios'
+import store from '../store'
 
 Vue.use(VueI18n)
 
-const { locale, translations } = window.config
-
 const i18n = new VueI18n({
-  locale,
-  messages: {
-    [locale]: translations
-  }
+  locale: window.config.i18n.defaultLocale,
+  messages: {}
 })
+
+/**
+ * @param {String} locale
+ */
+export async function loadMessages (locale) {
+  if (Object.keys(i18n.getLocaleMessage(locale)).length === 0) {
+    const { translations } = window.config.i18n
+    if (locale in translations) {
+      i18n.setLocaleMessage(locale, translations[locale])
+    } else {
+      const { data } = await axios.get(`/api/translations/locale/${locale}`)
+      i18n.setLocaleMessage(locale, data)
+    }
+  }
+
+  if (i18n.locale !== locale) {
+    i18n.locale = locale
+  }
+}
+
+(async () => {
+  await loadMessages(store.getters.locale)
+})()
 
 export default i18n
