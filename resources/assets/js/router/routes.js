a20d273a52d0c9deb840c62a58bb337a14f23ba3
Goopil <zac@goopil.org>
2018-03-16 23:12:33 +0100

    async routing

--- resources/assets/js/router/routes.js
+++ resources/assets/js/router/routes.js
@@ -5,16 +5,53 @@ export default [
 
   {
     path: 'settings',
-    component: require('~/pages/settings/index.vue'),
+    component: () => import(/* webpackChunkName: "auth" */ '~/pages/settings/index.vue'),
     children: [
       { path: '', redirect: { name: 'settings.profile' } },
-      { path: 'profile', name: 'settings.profile', component: require('~/pages/settings/profile.vue') },
-      { path: 'password', name: 'settings.password', component: require('~/pages/settings/password.vue') }
+      {
+        path: 'profile',
+        name: 'settings.profile',
+        component: () => import(/* webpackChunkName: "auth" */ '~/pages/settings/profile.vue')
+      },
+      {
+        path: 'password',
+        name: 'settings.password',
+        component: () => import(/* webpackChunkName: "auth" */ '~/pages/settings/password.vue')
+      }
     ]
   },
 
-  { path: 'login', name: 'login', component: require('~/pages/auth/login.vue') },
-  { path: 'register', name: 'register', component: require('~/pages/auth/register.vue') },
-  { path: 'password/reset', name: 'password.request', component: require('~/pages/auth/password/email.vue') },
-  { path: 'password/reset/:token', name: 'password.reset', component: require('~/pages/auth/password/reset.vue') },
+  { path: 'login', name: 'login', component: () => import(/* webpackChunkName: "auth" */ '~/pages/auth/login.vue') },
+  {
+    path: 'register',
+    name: 'register',
+    component: () => import(/* webpackChunkName: "auth" */ '~/pages/auth/register.vue')
+  },
+  {
+    path: 'password/reset',
+    name: 'password.request',
+    component: () => import(/* webpackChunkName: "auth" */ '~/pages/auth/password/email.vue')
+  },
+  {
+    path: 'password/reset/:token',
+    name: 'password.reset',
+    component: () => import(/* webpackChunkName: "auth" */  '~/pages/auth/password/reset.vue')
+  },
+  {
+    path: 'admin/users',
+    component: () => import(/* webpackChunkName: "admin.users" */ '~/pages/users/index.js'),
+    children: [
+      {
+        path: '',
+        name: 'admin.users',
+        // meta: { keepAlive: true },
+        component: () => import(/* webpackChunkName: "admin.users" */ '~/pages/users/list')
+      },
+      {
+        path: ':id',
+        name: 'admin.users.item',
+        component: () => import(/* webpackChunkName: "admin.users" */ '~/pages/users/item')
+      }
+    ]
+  }
 ]
