b50a5b5db2dde7abf8838463f5fb5733ff2516b0
Goopil <zac@goopil.org>
2018-03-17 23:02:51 +0100

    move user store in his own file and extract mutation types

---
+++ resources/assets/js/modules/users/store/types.js
@@ -0,0 +1,9 @@
+// users
+export const SET_USERS = 'SET_USERS'
+export const REPLACE_USER = 'REPLACE_USER'
+export const CONCAT_USERS = 'CONCAT_USERS'
+export const DELETE_USER = 'DELETE_USER'
+
+// roles
+export const SET_ROLES = 'SET_ROLES'
+export const CONCAT_ROLES = 'CONCAT_ROLES'
---
+++ resources/assets/js/modules/users/store/user.js
@@ -0,0 +1,69 @@
+import axios from 'axios'
+import * as types from './types'
+
+export const state = {
+  all: []
+}
+
+export const mutations = {
+  [types.SET_USERS] (state, data) {
+    state.all = data
+  },
+
+  [types.CONCAT_USERS] (state, data) {
+    state.all = [...new Set([...state.all, ...data])]
+  },
+
+  [types.DELETE_USER] (state, user) {
+    const index = state.all.findIndex(u => u.id === user.id)
+    state.all.splice(index, 1)
+  },
+
+  [types.REPLACE_USER] (state, user) {
+    const index = state.all.findIndex(u => u.id === user.id)
+    state.all.splice(index, 1, user)
+  }
+}
+
+export const actions = {
+  async fetchUsers ({ dispatch }, force = false) {
+    let { users } = window.__state__
+    if (users === undefined || users.length < 1 || force === true) {
+      const { data } = await axios.get('/api/users')
+      users = data
+    }
+
+    await dispatch('setUsers', users)
+  },
+
+  setUsers ({ commit }, data) {
+    return new Promise(resolve => {
+      commit(types.SET_USERS, data)
+      resolve()
+    })
+  },
+
+  async fetchUserDetail ({commit, getters, rootState}, force = false) {
+    let { user } = window.__state__
+    if (user === undefined || user.length < 1 || force === true) {
+      const { data } = await axios.get('/api/users/' + getters.userByRoute)
+      user = data
+    }
+
+    commit(types.REPLACE_USER, user)
+  },
+
+  async deleteUser ({ commit, dispatch }, user) {
+    await axios.delete('/api/users/' + user.id)
+    commit(types.DELETE_USER, user)
+    await dispatch('responseMessage', {
+      type: 'success',
+      text: `User ${user.name} deleted`
+    })
+  }
+}
+
+export const getters = {
+  users: state => state.all,
+  userByRoute: (state, getters, rootState) => state.all.find(u => u.id === parseInt(rootState.route.params.id))
+}
