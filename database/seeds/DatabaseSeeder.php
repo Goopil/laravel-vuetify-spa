3d485714374d564d5bb11f8406b39443fed32d1e
Goopil <zac@goopil.org>
2018-03-17 17:22:08 +0100

    populate default role to user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(LanguageLinesTableSeeder::class);
+        $this->call(RoleSeeder::class);
         factory(\App\User::class)->create(['email' => 'zac@goopil.org']);
     }
 }
