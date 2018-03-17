3b009fe4c37cdfa6dcde196e92eb3106a01d1439
Goopil <zac@goopil.org>
2018-03-17 10:06:33 +0100

    fix min password length

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('dev12345'),
+        'password' => $password ?: $password = bcrypt('secret12345'),
         'remember_token' => str_random(10),
     ];
 });
